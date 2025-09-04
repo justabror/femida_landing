becaab304ab811c28559f0df7642678bdfd0e650
abror <abror.karimxonov@ssd.com>
2025-09-04 14:00:38 +0500

    seo

--- src/shared/config/seo.ts
+++ src/shared/config/seo.ts
@@ -1,5 +1,5 @@
 import { Metadata, Viewport } from "next";
-import { getLocale } from "next-intl/server";
+import { getLocale, getTranslations } from "next-intl/server";
 import { Geologica } from "next/font/google";
 
 export const SystemFont = Geologica({
@@ -9,14 +9,46 @@ export const SystemFont = Geologica({
 });
 
 export async function metadataBase(): Promise<Metadata> {
-  // const t = await getTranslations();
+  const t = await getTranslations();
   const locale = await getLocale();
 
   return {
     title: "Femida",
-    description: "Femida - Your Trusted Legal Partner",
-    keywords: [],
-    // metadataBase: new URL("example.com"),
+    description: "Femida - Юридик хизматлар",
+    keywords: [
+      "Femida",
+      "Фемида",
+      "Yuridik xizmatlar",
+      "Юридические услуги",
+      "Legal services",
+      "Advokat",
+      "Адвокат",
+      "Lawyer",
+      "Huquqshunos",
+      "Правовед",
+      "Legal advisor",
+      "Yuridik maslahat",
+      "Юридическая консультация",
+      "Legal consultation",
+      "Shaxsiy ma'muriy huquq",
+      "Гражданское право",
+      "Civil law",
+      "Jinoyat huquqi",
+      "Уголовное право",
+      "Criminal law",
+      "Oilaviy huquq",
+      "Семейное право",
+      "Family law",
+      "Mehnat huquqi",
+      "Трудовое право",
+      "Labor law",
+      "Fuqarolik huquqi",
+      "Гражданское право",
+      "Commercial law",
+      "Iqtisodiy huquq",
+      " Экономическое право",
+    ],
+    metadataBase: new URL("https://femida.uz/"),
     alternates: {
       canonical: `/${locale}`,
       languages: {
@@ -25,29 +57,29 @@ export async function metadataBase(): Promise<Metadata> {
         "ru-RU": "/ru",
       },
     },
-    // openGraph: {
-    // title: t("seo.title"),
-    // description: t("seo.description"),
-    // url: "https://example.com",
-    // type: "website",
-    // images: "/android-chrome-192x192.png",
-    // siteName: "Femida",
-    // locale: locale === "en" ? "en_US" : locale === "ru" ? "ru_RU" : "uz_UZ",
-    // },
-    // icons: {
-    //   icon: "/favicon-16x16.png",
-    //   shortcut: "/favicon-32x32.png",
-    //   apple: "/apple-touch-icon.png",
-    //   other: {
-    //     rel: "icon",
-    //     url: "/android-chrome-192x192.png",
-    //   },
-    // },
-    // manifest: "/manifest.json",
-    // verification: {
-    //   google: "google-site-verification-code",
-    //   yandex: "yandex-verification-code",
-    // },
+    openGraph: {
+      title: "Femida",
+      description: "Femida - Юридик хизматлар",
+      url: "https://femida.uz/",
+      type: "website",
+      images: "/android-chrome-192x192.png",
+      siteName: "femida",
+      locale: locale === "en" ? "en_US" : locale === "ru" ? "ru_RU" : "uz_UZ",
+    },
+    icons: {
+      icon: "/favicon-16x16.png",
+      shortcut: "/favicon-32x32.png",
+      apple: "/apple-touch-icon.png",
+      other: {
+        rel: "icon",
+        url: "/android-chrome-192x192.png",
+      },
+    },
+    manifest: "/manifest.json",
+    verification: {
+      google: "3BwJoP3BiGVMrtczgRmLBkXe7q8_uHdwi1VYGhOMx24",
+      yandex: "bf75b24851bc7476",
+    },
     category: "business",
     pinterest: {
       richPin: true,
@@ -56,10 +88,10 @@ export async function metadataBase(): Promise<Metadata> {
 }
 
 export const viewportBase: Viewport = {
-  // themeColor: "#100e2a",
-  // colorScheme: "dark",
-  // width: "device-width",
-  // initialScale: 1,
-  // maximumScale: 1,
-  // userScalable: true,
+  themeColor: "#100e2a",
+  colorScheme: "dark",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: true,
 };
